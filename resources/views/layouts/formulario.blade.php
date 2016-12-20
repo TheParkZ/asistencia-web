66b66544d3485063a8dbff269191214f55f3a700
Alfredo Amaut Garcia <Alfredo Amaut Garcia>
2016-12-20 17:29:59 -0500

    First Commit

---
+++ resources/views/layouts/formulario.blade.php
@@ -0,0 +1,28 @@
+<form class="form-horizontal">
+  <div class="form-group">
+    	<label for="titulo" class="col-sm-2 control-label">Titulo</label>
+    	<div class="col-sm-10">
+      		<input type="email" class="form-control" name="titulo" placeholder="Titulo">
+    	</div>
+	</div>
+
+  <div class="form-group">
+    	<label for="descripcion" class="col-sm-2 control-label">Descripcion</label>
+    	<div class="col-sm-10">
+      		<textarea type="text" class="form-control" nane="descripcion" placeholder="Descripcion"/>
+   		 </div>
+  </div>
+
+  <div class="form-group">
+    	<label for="imagen" class="col-sm-2 control-label">Imagen</label>
+    	<div class="col-sm-10">
+      		<input type="file" class="form-control" nane="urlImg">
+   		 </div>
+  </div>
+
+  <div class="form-group">
+    	<div class="col-sm-offset-2 col-sm-10">
+      		<button type="submit" class="btn btn-default">Sign in</button>
+    	</div>
+  </div>
+</form>
